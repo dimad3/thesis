2953b5b45088dade736dc3ea6d156f86e0c7401e
dimad3 <dmitrijs633@gmail.com>
2020-06-19 22:28:33 +0300

    User register 3

--- public/register.php
+++ public/register.php
@@ -4,48 +4,49 @@ require_once 'init.php';
 // L#8
 // Input::isSubmited() - check whether the form was submited (see forum)
 // Returns TRUE if it was and FALSE if it was NOT
-if (Input::isSubmited()) { // true or false
+$form_submited = Input::isSubmited();
+if ($form_submited == true) { // true or false
     
-  // L#9 05:10 - get a VALUE of a form's field after the form was submited
-  // 'token' - the name of the form's field from which to get the VALUE
-  $fieldValue = Input::get('token');  // true or false
+	// L#9 05:10 - get a VALUE of a form's field after the form was submited
+	// 'token' - the name of the form's field from which to get the VALUE
+	$fieldValue = Input::get('token');  // true or false
 
-  // check whether `form's token value` exists in the `$_SESSION[] array`
-  $tokenExists = Token::check($fieldValue); // true or false
+	// check whether `form's token value` exists in the `$_SESSION[] array`
+	$tokenExists = Token::check($fieldValue); // true or false
+	
+	if ($tokenExists == true) { // L#9 
   
-  if ($tokenExists == true) { // L#9 
-  
-    $validation = new Validate();
+		$validation = new Validate();
 
-    $validation = $validation->check(
-        $_POST,
-        [
-            'username' => [                 // field name
-                'required'  => true,
-                'min'       => 3,
-                'max'       => 12,
-                'unique'    => 'users'      // the name of the table
-            ],
-            
-            'email' =>  [                   // 'email'field name
-                'required'  =>  true,
-                'email'     =>  true,       // 'email' rule name
-                'unique'    =>  'users'     // the name of the table
-            ],
+		$validation = $validation->check($_POST,
+			[
+				'username' => [                 // field name
+					'required'  => true,
+					'min'       => 3,
+					'max'       => 12,
+					'unique'    => 'users'      // the name of the table
+				],
+				
+				'email' =>  [                   // 'email'field name
+					'required'  =>  true,
+					'email'     =>  true,       // 'email' rule name
+					'unique'    =>  'users'     // the name of the table
+				],
 
-            'password' => [                 // field name
-                'required'  => true,
-                'min'       => 3
-            ],
+				'password' => [                 // field name
+					'required'  => true,
+					'min'       => 3
+				],
 
-            'password_again' => [           // field name
-                'required'  => true,
-                'matches'   => 'password'   // the name of the field to compare with
-            ]
-        ]
-    );
+				'password_again' => [           // field name
+					'required'  => true,
+					'matches'   => 'password'   // the name of the field to compare with
+				]
+			]
+		);
+		// Returns `Validate object`
+	}
 
-    // $validation = Validate object
     // check whether `$passed property` of `Validate object` is TRUE
     if ($validation->passed()) {
         
@@ -72,17 +73,7 @@ if (Input::isSubmited()) { // true or false
 
         // set new element in the $_SESSION[] array
         Session::flash('success', 'register success');
-        // echo 'passed';
-        // header('location: test.php');
-    } else {
-        foreach($validation->errors() as $error) {
-            // $validation->errors() - ARRAY - `$errors property` of `Validate object`
-            // $error - VALUE of each error
-            echo $error . '</br>';
-        }
-    }
-  }
-
+	}
 }
 
 $custom_css = 'style1.css';  // ok
